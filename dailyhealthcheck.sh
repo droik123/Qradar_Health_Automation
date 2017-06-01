fc66becfac76980b3e30519f3d23e65495e39c01
droik123 <ashis.sahoo@gmail.com>
2017-06-01 13:54:59 +0530

    Update dailyhealthcheck.sh

--- dailyhealthcheck.sh
+++ dailyhealthcheck.sh
@@ -15,7 +15,7 @@ ls -lt /store/backup |head -5 >>healthcheck.out
 printf "\n*****User Check***** \n\n" >>healthcheck.out
 ls -l /store/users  >>healthcheck.out
 cp healthcheck.out "healthcheck.out.$(date +%Y%m%d)" && rm -rf healthcheck.out
-mail -s "$(hostname) Daily System Check $(date +%Y%m%d)" klsoc@paladion.net < healthcheck.out.$(date +%Y%m%d)
+mail -s "$(hostname) Daily System Check $(date +%Y%m%d)" youremail@male.cim < healthcheck.out.$(date +%Y%m%d)
 
 
 else if [[ $(/opt/qradar/ha/bin/ha_getstate.sh) = standby ]]; then
